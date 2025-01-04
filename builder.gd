02d6290625e86c00ff76c8cacd1b4a28c4a7f254
Kurooco <128552915+Kurooco@users.noreply.github.com>
2025-01-04 14:46:55 -0500

    made collision invisible, added borders to grid, and prevented overlap of pieces

--- builder.gd
+++ builder.gd
@@ -11,6 +11,7 @@ const GRID_HEIGHT = 50
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	move_and_place(1)
 	for i in 10:
 		move_random_and_place()
 
@@ -18,17 +19,27 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func move_and_place(dir):
+	var direction = move(dir)
 	
-func move_random_and_place():
-	var direction = move_random()
+	# Check to make sure this spot is not occupied
+	var placement_position = global_position + (direction_map[direction] * (GRID_HEIGHT if direction % 2 == 0 else GRID_WIDTH)/-2)
+	if(taken_spots.has(placement_position)):
+		return
+	
+	# Place piece
 	var new_piece
 	if(direction % 2 == 0):
 		new_piece = pole.instantiate()
 	else:
 		new_piece = platform.instantiate()
-		
-	new_piece.position = position + (direction_map[direction] * (GRID_HEIGHT if direction % 2 == 0 else GRID_WIDTH)/-2)
+	new_piece.global_position = placement_position
+	taken_spots[placement_position] = new_piece
 	worldmap.add_child.call_deferred(new_piece)
+	
+func move_random_and_place():
+	move_and_place(randi()%4)
 	
 func move_random():
 	var direction = randi()%4;
@@ -36,9 +47,12 @@ func move_random():
 	return direction
 
 func move(direction):
-	position += direction_map[direction] * (GRID_HEIGHT if direction % 2 == 0 else GRID_WIDTH)
+	var prev_position = Vector2(global_position)
+	var size = get_viewport_rect().size
+	global_position += direction_map[direction] * (GRID_HEIGHT if direction % 2 == 0 else GRID_WIDTH)
+	if global_position.x > size.x || global_position.x < -size.x || global_position.y > size.y || global_position.y < -size.y:
+		global_position = prev_position
 	return direction
-
 
 func _on_timer_timeout():
 	move_random_and_place()
--- player.gd
+++ player.gd
@@ -2,23 +2,28 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -320.0
+const TERMINAL_VELOCITY = 150
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var has_jump = true
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		if(velocity.y > TERMINAL_VELOCITY):
+			velocity.y = TERMINAL_VELOCITY
 
 	# Handle jump.
-	if Input.is_action_just_pressed("up") and is_on_floor():
+	if(is_on_floor()):
+		has_jump = true
+	if Input.is_action_pressed("up") and has_jump:
 		velocity.y = JUMP_VELOCITY
+		has_jump = false
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED

d2ebf2ef2eda88b693ef96db7e3d7e5760419c74
Kurooco <128552915+Kurooco@users.noreply.github.com>
2025-01-04 14:07:50 -0500

    basic grid movement and piece placement

--- builder.gd
+++ builder.gd
@@ -4,26 +4,41 @@ extends Node2D
 @export var pole : PackedScene
 @export var worldmap : Node
 var taken_spots = {}
+var direction_map = {0: Vector2.UP, 1: Vector2.RIGHT, 2: Vector2.DOWN, 3: Vector2.LEFT}
 
 const GRID_WIDTH = 50
 const GRID_HEIGHT = 50
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position = Vector2(100, 100)
 	for i in 10:
-		move_random()
-		var newplat = platform.instantiate()
-		newplat.position = position
-		worldmap.add_child.call_deferred(newplat)
+		move_random_and_place()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 	
+func move_random_and_place():
+	var direction = move_random()
+	var new_piece
+	if(direction % 2 == 0):
+		new_piece = pole.instantiate()
+	else:
+		new_piece = platform.instantiate()
+		
+	new_piece.position = position + (direction_map[direction] * (GRID_HEIGHT if direction % 2 == 0 else GRID_WIDTH)/-2)
+	worldmap.add_child.call_deferred(new_piece)
+	
 func move_random():
 	var direction = randi()%4;
-	var direction_map = {0: Vector2.UP, 1: Vector2.RIGHT, 2: Vector2.DOWN, 3: Vector2.LEFT}
+	move(direction)
+	return direction
+
+func move(direction):
 	position += direction_map[direction] * (GRID_HEIGHT if direction % 2 == 0 else GRID_WIDTH)
-	return direction_map[direction]
+	return direction
+
+
+func _on_timer_timeout():
+	move_random_and_place()

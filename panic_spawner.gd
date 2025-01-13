7273f0ddbc3ff259fda3020e3e2b94d873ae8abf
Kurooco <128552915+Kurooco@users.noreply.github.com>
2025-01-12 20:27:38 -0600

    can delete platforms underneath player

--- panic_spawner.gd
+++ panic_spawner.gd
@@ -8,7 +8,7 @@ var panic : PackedScene = load("res://panic.tscn")
 func _ready():
 	for i in amount:
 		var new_p = panic.instantiate()
-		new_p.position = Vector2(randi()%100, position.y)
+		new_p.position = Vector2(randi()%500 - 250, position.y)
 		worldmap.add_child.call_deferred(new_p)
 
 
--- platform.gd
+++ platform.gd
@@ -8,7 +8,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if(Input.is_action_just_pressed("down") && !$PlayerDestroy.get_overlapping_bodies().is_empty()):
+		destroy(position, 100)
 
 
 func destroy(p, r):

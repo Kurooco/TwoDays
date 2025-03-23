9a1f789ea3798dc4cad2378af0e420324e5c667d
Kurooco <128552915+Kurooco@users.noreply.github.com>
2025-03-23 14:36:32 -0400

    added effects to menu/lose/win screens

---
+++ scenes/lose_screen.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+var tween
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Panel.modulate.a = 0
+	$Panel.scale = Vector2()
+	tween = get_tree().create_tween()
+	tween.tween_property($Panel, "modulate", Color.WHITE, 1).set_trans(Tween.TRANS_SINE)
+	tween.parallel().tween_property($Panel, "scale", Vector2(1, 1), 1).set_trans(Tween.TRANS_SINE)
+	tween.set_loops(5)
+	#tween.tween_callback($Panel.queue_free)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass

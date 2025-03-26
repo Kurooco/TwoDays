7f65eb20bd5ec3cf8770db82c5114dce21823e99
Kurooco <128552915+Kurooco@users.noreply.github.com>
2025-03-26 11:34:25 -0500

    fixed scene buttons
    used string paths instead of packed scenes to avoid circular dependency issues

--- scripts/switch_screen_button.gd
+++ scripts/switch_screen_button.gd
@@ -1,6 +1,6 @@
 extends Button
 
-@export var scene : PackedScene
+@export var scene : String
 @export_enum("None", "Replace Previous", "Clear All", "Go Back") var replacement_mode: int
 @export var hide_previous_screen = false
 @export var set_pause : bool
@@ -9,12 +9,12 @@ func _on_button_down():
 	print_debug(scene)
 	if(replacement_mode == 0):
 		Autoload.game_manager.pause_current(set_pause)
-		Autoload.game_manager.add_scene_to_stack(scene, hide_previous_screen)
+		Autoload.game_manager.add_scene_to_stack(load(scene), hide_previous_screen)
 	if(replacement_mode == 1):
-		Autoload.game_manager.replace_with_scene(scene)
+		Autoload.game_manager.replace_with_scene(load(scene))
 	if(replacement_mode == 2):
 		Autoload.game_manager.clear_stack()
-		Autoload.game_manager.add_scene_to_stack(scene, hide_previous_screen)
+		Autoload.game_manager.add_scene_to_stack(load(scene), hide_previous_screen)
 	if(replacement_mode == 3):
 		Autoload.game_manager.pop()
 		Autoload.game_manager.pause_current(set_pause)

954805ca40f79d1119ae01328c7fd5912c6768bf
Kurooco <128552915+Kurooco@users.noreply.github.com>
2025-03-27 18:59:47 -0500

    fixed lag with RigidBody platforms

--- scripts/platform.gd
+++ scripts/platform.gd
@@ -1,19 +1,18 @@
 extends RigidBody2D
 
+@export var is_horizontal_platform : bool
 var FADE_RATE = 0.5
-
 var is_destroyed = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Sprite2D.frame = randi()%4
-	set_physics_process(false)
-	pass # Replace with function body.
+	call_deferred("set_freeze_enabled", true)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if(Input.is_action_just_pressed("down") && !$PlayerDestroy.get_overlapping_bodies().is_empty()):
+	if(is_horizontal_platform && Input.is_action_pressed("down") && !$PlayerDestroy.get_overlapping_bodies().is_empty()):
 		destroy(position, 100)
 	if(is_destroyed):
 		#print_debug($Sprite2D.modulate.a)
@@ -24,11 +23,13 @@ func _process(delta):
 
 func destroy(p, r):
 	if(p.distance_to(position) < r):
+		call_deferred("set_freeze_enabled", false)
 		set_collision_layer_value(2, false)
 		set_collision_layer_value(3, false)
 		gravity_scale = 1
-		apply_impulse(p.direction_to(position)*200)
+		#apply_impulse(p.direction_to(position)*200)
+		call_deferred("apply_impulse", p.direction_to(position)*200)
+		call_deferred("set_angular_velocity", randi_range(-5, 5))
 		$CollisionShape2D.set_deferred("one_way_collision", false)
 		is_destroyed = true
-		set_physics_process(true)
 		#queue_free()
